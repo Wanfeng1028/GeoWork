045335058fbb20c25a6e3764e9016a312e1a34d0
Wanfeng1028 <wanfeng1028@gmail.com>
2026-08-15 19:19:06 +0800

    chore: 关闭 commitlint subject-case 规则以兼容中文提交

--- commitlint.config.js
+++ commitlint.config.js
@@ -6,6 +6,8 @@ module.exports = {
       'always',
       ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'merge', 'revert'],
     ],
+    // 中文提交信息常以英文专有名词开头（CI、Go、ChatComposer 等），关闭大小写检查
+    'subject-case': [0],
   },
   ignores: [(commit) => /^Merge\s/.test(commit)],
 }
